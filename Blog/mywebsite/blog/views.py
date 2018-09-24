e5d47b2c7ad852211274a638db4ba0ab8f469af0
Daggron <returnofking04@gmail.com>
2018-09-24 23:49:23 +0530

    New  Buttons
    New Buttons  added to  add or  edit your  Blog

--- Blog/mywebsite/blog/views.py
+++ Blog/mywebsite/blog/views.py
@@ -1,10 +1,12 @@
-from django.shortcuts import render,get_object_or_404
+from django.shortcuts import render,redirect
 from .models import Post
 from django.utils import timezone
+from . forms import PostForm
+from django.shortcuts import get_object_or_404
 
 
 def post_list(request):
-    posts =Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    posts =Post.objects.filter(published_date__lte=timezone.now()).order_by('created_date')
     return render(request,'blog/index.html', {'posts': posts})
 
 
@@ -14,3 +16,33 @@ def post_details(request,pk):
     except Post.DoesNotExist:
         return render(request,'blog/error.html',{})
     return render(request, 'blog/post_details.html',  {'post': post})
+
+
+def post_new(request):
+    if request.method == "POST":
+        form = PostForm(request.POST)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.published_date = timezone.now()
+            post.save()
+            return redirect('post_details', pk=post.pk)
+    else:
+        form = PostForm()
+    return render(request, 'blog/post_edit.html', {'form': form})
+
+
+def post_edit(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    if request.method == "POST":
+        form = PostForm(request.POST, instance=post)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.published_date = timezone.now()
+            post.save()
+            post.publish()
+            return redirect('post_details', pk=post.pk)
+    else:
+        form = PostForm(instance=post)
+    return render(request, 'blog/post_edit.html', {'form': form})
